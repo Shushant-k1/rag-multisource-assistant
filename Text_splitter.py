b7807886b891f50c381d40fe2eb5a649eaa0516c
shushant kumar <rekhaubgb@gmail.com>
2025-07-14 16:29:52 +0530

    Update Text_splitter.py

--- Text_splitter.py
+++ Text_splitter.py
@@ -1,1 +1,13 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+def text_splitter(text):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=100,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
+
+    split_docs = splitter.create_documents([text])
+    chunks = [doc.page_content for doc in split_docs]
+    return chunks
